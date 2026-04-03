c385b3867422ea13644c065de7c5529a4f55e0b4
Mirotin Artem <ai.mirotin@gmail.com>
2026-04-04 00:44:11 +0300

    feat: export pages from main index; include src/pages in build tsconfig

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export * from "./primitives";
 export * from "./components";
 export * from "./compositions";
 export * from "./layout";
+export * from "./pages";
 export * from "./types";
 export { cn } from "./lib/utils";
 export type { Status } from "./tokens/colors";
